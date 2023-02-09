5f4e01ea9e3b12a09ecffe8f0e883df3e67a2208
LagacheLouis <louislagache@gmail.com>
2023-02-09 02:16:02 +0100

    layermask field

--- Editor/EditorHelpers.cs
+++ Editor/EditorHelpers.cs
@@ -96,6 +96,37 @@ namespace llagache.Editor
             }
         }
         
+        static List<int> layerNumbers = new List<int>();
+ 
+        public static LayerMask LayerMaskField(Rect r, string label, LayerMask layerMask)
+        {
+            var layers = InternalEditorUtility.layers;
+ 
+            layerNumbers.Clear();
+ 
+            for (int i = 0; i < layers.Length; i++)
+                layerNumbers.Add(LayerMask.NameToLayer(layers[i]));
+ 
+            int maskWithoutEmpty = 0;
+            for (int i = 0; i < layerNumbers.Count; i++)
+            {
+                if (((1 << layerNumbers[i]) & layerMask.value) > 0)
+                    maskWithoutEmpty |= (1 << i);
+            }
+ 
+            maskWithoutEmpty = EditorGUI.MaskField(r, label, maskWithoutEmpty, layers);
+ 
+            int mask = 0;
+            for (int i = 0; i < layerNumbers.Count; i++)
+            {
+                if ((maskWithoutEmpty & (1 << i)) > 0)
+                    mask |= (1 << layerNumbers[i]);
+            }
+            layerMask.value = mask;
+ 
+            return layerMask;
+        }
+        
     }
 }
 
